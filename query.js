9bf141fb35c1ce83ba69118789613a8670af2d65
velsanjay <sanjaymech2313@mail.com>
2023-04-22 11:52:37 +0530

    mongodb day-2

---
+++ query.js
@@ -0,0 +1,409 @@
+
+db.zenclass.insertMany([
+    {
+        "id": 1,
+        "user": [
+            {
+                "name": "Sanjay",
+                "placement": true,
+                "codecatta":532,
+                "attendence":57,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "10-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "19-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "27-10-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "11-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "16-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Mathi",
+                "codecatta":387,
+                "placement": true,
+                "attendence":54,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "11-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "01-11-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "02-11-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "09-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "16-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Thillai",
+                "placement": true,
+                "codecatta":428,
+                "attendence":52,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "08-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "18-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "01-11-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "12-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "17-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Prabanjan",
+                "placement": false,
+                "codecatta":487,
+                "attendence":59,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "09-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "31-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "04-11-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "08-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "18-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Dhinesh",
+                "placement": false,
+                "attendence":60,
+                "codecatta":522,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "11-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "21-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "30-10-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "13-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "19-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Siranjeevi",
+                "placement": false,
+                "codecatta":232,
+                "attendence":48,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "09-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "19-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "29-10-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "13-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "20-11-2022"
+                    }
+                ]
+            },
+            {
+                "name": "Sanakkiyan",
+                "placement": true,
+                "codecatta":542,
+                "attendence":56,
+                "task": [
+                    {
+                        1: "calculator",
+                        "deadLine": "09-10-2022",
+                        "submited": "11-10-2022"
+                    },
+                    {
+                        2: "Form",
+                        "deadLine": "20-10-2022",
+                        "submited": "31-10-2022"
+                    },
+                    {
+                        3: "Bootstrap",
+                        "deadLine": "30-10-2022",
+                        "submited": "05-11-2022"
+                    },
+                    {
+                        4: "CRUD",
+                        "deadLine": "10-11-2022",
+                        "submited": "10-11-2022"
+                    },
+                    {
+                        5: "CRUD Content",
+                        "deadLine": "17-11-2022",
+                        "submited": "18-11-2022"
+                    }
+                ]
+            }
+        ],
+        "Topics":[
+            {
+                "name":"Java Script",
+                "completd":'08-10-2022',
+                "mendar":"Kalai"
+            },
+            {
+                "name":"XMLHTTPRequest",
+                "completd":'15-10-2022',
+                "mendar":"Aravind"
+            },
+            {
+                "name":"HTML",
+                "completd":'20-10-2022',
+                "mendar":"Rajadurai"
+            },
+            {
+                "name":"CSS",
+                "completd":'25-10-2022',
+                "mendar":"Ragupathi"
+            },
+            {
+                "name":"Bootstrap",
+                "completd":'30-10-2022',
+                "mendar":"Ramya"
+            },
+            {
+                "name":"Advance JS",
+                "completd":'04-11-2022',
+                "mendar":"Usha"
+            },
+            {
+                "name":"React JS",
+                "completd":'09-11-2022',
+                "mendar":"Sanjay"
+            },
+            {
+                "name":"Database & Node JS",
+                "completd":'14-10-2022',
+                "mendar":"Deepika"
+            },
+            {
+                "name":"Full Stack Project Demo",
+                "completd":'20-11-2022',
+                "mendar":"Kalai"
+            }
+        ],
+        "company_drivers":[
+            {
+                "name":"Muthu",
+                "From":"01-10-2022",
+                "To":"10-10-2022",
+                "placement":true
+            },
+            {
+                "name":"Murali",
+                "From":"05-10-2022",
+                "To":"15-10-2022",
+                "placement":true
+            },
+            {
+                "name":"Kandeepan",
+                "From":"10-10-2022",
+                "To":"20-10-2022",
+                "placement":true
+            },
+            {
+                "name":"Vignesh",
+                "From":"15-10-2022",
+                "To":"25-10-2022",
+                "placement":false
+            },
+            {
+                "name":"Abdul",
+                "From":"20-10-2022",
+                "To":"30-10-2022",
+                "placement":true
+            },
+            {
+                "name":"Presana",
+                "From":"25-10-2022",
+                "To":"04-11-2022",
+                "placement":false
+            },
+            {
+                "name":"Ragunath",
+                "From":"30-10-2022",
+                "To":"09-11-2022",
+                "placement":false
+            },
+            {
+                "name":"Prabakaran",
+                "From":"04-11-2022",
+                "To":"14-11-2022",
+                "placement":true
+            },
+            {
+                "name":"Chandru",
+                "From":"09-11-2022",
+                "To":"19-11-2022",
+                "placement":true
+            },
+            {
+                "name":"Magesh",
+                "From":"14-11-2022",
+                "To":"24-11-2022",
+                "placement":false
+            }
+        ],
+        "mendars":[
+            {
+                "name":"Kalai",
+                "No_of_Student":20
+            },
+            {
+                "name":"Aravind",
+                "No_of_Student":32
+            },
+            {
+                "name":"Rajadurai",
+                "No_of_Student":10
+            },
+            {
+                "name":"Ragupathi",
+                "No_of_Student":27
+            },
+            {
+                "name":"Ramya",
+                "No_of_Student":25
+            },
+            {
+                "name":"Usha",
+                "No_of_Student":10
+            },
+            {
+                "name":"Deepika",
+                "No_of_Student":11
+            },
+            {
+                "name":"Sanjay",
+                "No_of_Student":15
+            }
+        ]
+    }
+])
+
+// 1.Find all the topics and tasks which are thought in the month of October
+
+db.zenclass.find({},{topics:{completd:{$lt:"31-10-2022"}},user:{task:{submited:{$lt:"31-10-2022"}}}})
+
+
+// 2.Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+
+db.zenclass.find({},{company_drivers:{from:{$gt:"15-10-2022", $lt:"30-15-2022"}}})
+
+
+// 3.Find all the company drives and students who are appeared for the placement.
+
+db.zenclass.find({},{company_drivers:{placement:true},user:{placement:true}})
+
+
+// 4.Find the number of problems solved by the user in codekata
+
+db.zenclass.find({},{user:{name:true, codecatta:true}})
+
+// 5.Find all the mentors with who has the mentee's count more than 15
+
+db.zenclass.find({},{mendar:{No_of_Student:{$gte:15}}})
+
+// 6.Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+
+db.zenclass.find({},{user:{task:{submited:{$lt:"31-10-2022"}}}})
